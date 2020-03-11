6b61488d5fd7d4b3fb214d175b30765bb13c36c1
Bill <billcshto@gmail.com>
2020-03-11 00:26:19 -0400

    add ball-launch sim

---
+++ ball-launch/constants.h
@@ -0,0 +1,15 @@
+//
+// Created by Bill Chen on 2020-03-10.
+//
+
+#ifndef BALL_LAUNCH_CONSTANTS_H
+#define BALL_LAUNCH_CONSTANTS_H
+
+namespace myConstants
+{
+    double gravity{ -9.81 }; // in meters/second squared
+    double airResistance{ 0 };
+}
+
+
+#endif //BALL_LAUNCH_CONSTANTS_H
